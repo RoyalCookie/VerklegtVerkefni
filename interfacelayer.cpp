a0be412d168416276c92d7d9fcec6f96df3bdce0
TomasHelgason <tomashelgason92@gmail.com>
2014-12-01 13:36:34 +0000

    interface
    fyrsta uppfærsla frá tómasi

--- interfacelayer.cpp
+++ interfacelayer.cpp
@@ -1,9 +1,75 @@
 #include "interfacelayer.h"
+#include "servicelayer.h"
+using namespace std;
 
 interfaceLayer::interfaceLayer()
 {
 }
+void interfaceLayer::choose()
+{   char val;
+    bool invalid=false;
+    do {
+    cout << "Choose wisely \nA - Register a person \nB - Display \nC - Search \nQ - Quit\n";
+    cin >> val;
 
+    switch(val){
+    case ('A'|'a'):
+        skra();
+        break;
+    case ('B'|'b'):
+
+        break;
+    case ('C'|'c'):
+    leita();
+        break;
+    case ('Q'|'q'):
+    invalid=false;
+        break;
+    default :
+    cout << " Wrong input\n";
+    invalid=true;
+    }
+   }while(invalid==true);
+}
+void interfaceLayer::birta()
+{
+    char afram,type;
+    do{
+    cout << "N = Sort by last name then first name\n"
+         << "G = Sort by gender\n"
+         << "B = Sort by date of birth\n"
+         << "D = date of death\n";
+    cin >> type;
+    List(char type);
+    cout << "\nWould you like to display again?(Y/N)\n";
+    cin >> afram;
+    }while(afram=='y' || afram == 'Y');
+}
+void interfaceLayer::skra()
+{   char afram;
+    do{
+    Write();
+    cout << "\nWould you like to register another person?(Y/N)\n";
+    cin >> afram;
+    }while(afram=='y' || afram == 'Y');
+}
+void interfaceLayer::leita()
+{
+    char afram,type;
+    string terms;
+    do{
+    cout << "Enter search term: ";
+    cin >> terms;
+    cout << "N = Sort by last name then first name\n"
+         << "G = Sort by gender\n"
+         << "B = Sort by date of birth\n"
+         << "D = Sort by date of death\n";
+    cin >> type;
+    Search(string terms, char type);
+    cout << "\nWould you like to search again?(Y/N)\n";
+    cin >> afram;
+    }while(afram=='y' || afram == 'Y');
+}
 
 vector<person> interfaceLayer::search(string terms, char type){
     vector<person> listi = read();
--- interfacelayer.h
+++ interfacelayer.h
@@ -1,10 +1,19 @@
 #ifndef INTERFACELAYER_H
 #define INTERFACELAYER_H
 
+#include <iostream>
+#include <string>
+#include <vector>
+#include <list>
+
 class interfaceLayer
 {
 public:
     interfaceLayer();
+    void choose();
+    void skra();
+    void leita();
+    void birta();
 };
 
 #endif // INTERFACELAYER_H

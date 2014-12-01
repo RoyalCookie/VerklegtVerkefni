e84d3a706761b05dda3dc4851046f2bb978d9e5e
RoyalCookie <janus@Januss-MacBook-Pro.local>
2014-12-01 11:29:33 +0000

    helper.cpp vildi uppfæra

---
+++ helper.cpp
@@ -0,0 +1,13 @@
+#include "helper.h"
+
+using namespace std;
+
+
+//vector<person> Sort(vector<person> listi, char type){
+//    list<person> listToSort;
+//    for(int i = 0; i < listi.size; i++){
+//        listToSort.push_back(listi[i]);
+//    }
+//}
+//vector<person> Read();
+//vector<person> Write(vector<person> listi);
